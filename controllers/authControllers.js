be23501bd4b8fcaf4c66958bc18a8171e4f58ad6
khmdagal <khmdagal@gmail.com>
2024-08-14 20:26:45 +0100

    Add try catch blocks

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,7 +3,8 @@ const { promisify } = require('util')
 const jwt = require('jsonwebtoken')
 const pool = require('../utils/db/db');
 const { validationResult } = require('express-validator');
-const { correctPassword } = require('../middlewares/helper')
+const { correctPassword } = require('../middlewares/helper');
+const { error } = require('console');
 
 const createJwtToken = (id) => {
     const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
@@ -16,111 +17,131 @@ const sendToken = async () => {
 }
 
 exports.signUp = async (req, res, next) => {
-    // first checking the body request and validate
-    console.log("===Body data comes ===>>>", req.body)
-    const result = validationResult(req)
-    console.log(result)
-    if (!result.isEmpty()) {
-        const errors = result.array().map((el) => {
-            return {
-                message: el.msg,
-                field: el.path
-            }
-        })
+    try {
+        // first checking the body request and validate
+        console.log("===Body data comes ===>>>", req.body)
+        const result = validationResult(req)
+        console.log(result)
+        if (!result.isEmpty()) {
+            const errors = result.array().map((el) => {
+                return {
+                    message: el.msg,
+                    field: el.path
+                }
+            })
 
-        return res.status(400).json({
-            status: 'fail',
-            message: errors
-        })
-    }
+            return res.status(400).json({
+                status: 'fail',
+                message: errors
+            })
+        }
 
-    // Checking if the user already exist
-    const exitingUser = (await pool.query(`select username from users where username=$1`, [req.body.username])).rowCount
-    if (exitingUser !== 0) {
-        return res.status(400).json({
-            status: 'fail',
-            message: 'User already exists'
-        })
+        // Checking if the user already exist
+        const exitingUser = (await pool.query(`select username from users where username=$1`, [req.body.username])).rowCount
+        if (exitingUser !== 0) {
+            return res.status(400).json({
+                status: 'fail',
+                message: 'User already exists'
+            })
 
-    }
+        }
 
-    // hashing the password and store the DB hashed password
-    const hashedPassword = await bcrypt.hash(req.body.password, 12)
+        // hashing the password and store the DB hashed password
+        const hashedPassword = await bcrypt.hash(req.body.password, 12)
 
-    const cleanNewUserData = {
-        name: req.body.name, //1
-        username: req.body.username, //2
-        password: hashedPassword, //3
-        role: req.body.role, //4
-        school_id: req.body.school_id, //5
-        email: req.body.email, //6
-        approved: req.body.approved, //7
-        created_at: new Date() //8
-    }
+        const cleanNewUserData = {
+            name: req.body.name, //1
+            username: req.body.username, //2
+            password: hashedPassword, //3
+            role: req.body.role, //4
+            school_id: req.body.school_id, //5
+            email: req.body.email, //6
+            approved: req.body.approved, //7
+            created_at: new Date() //8
+        }
 
-    //creating the new user when the data passes the above checks
-    pool.query(`insert into users(name,username,password,role,school_id,email,approved,created_at) 
+        //creating the new user when the data passes the above checks
+        pool.query(`insert into users(name,username,password,role,school_id,email,approved,created_at) 
                 values($1,$2,$3,$4,$5,$6,$7,$8)             
     `, [cleanNewUserData.name,
-    cleanNewUserData.username,
-    cleanNewUserData.password,
-    cleanNewUserData.role,
-    cleanNewUserData.school_id,
-    cleanNewUserData.email,
-    cleanNewUserData.approved,
-    cleanNewUserData.created_at])
+        cleanNewUserData.username,
+        cleanNewUserData.password,
+        cleanNewUserData.role,
+        cleanNewUserData.school_id,
+        cleanNewUserData.email,
+        cleanNewUserData.approved,
+        cleanNewUserData.created_at])
 
 
-    res.status(201).json({
-        status: 'succuss',
-        data: cleanNewUserData
-    })
-    next()
+        res.status(201).json({
+            status: 'succuss',
+            data: cleanNewUserData
+        })
+        next()
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+        next(err)
+    }
+
+
 }
 
 exports.logIn = async (req, res, next) => {
-    const { username, password } = req.body;
+    try {
+        const { username, password } = req.body;
+        console.log(
+            `username ${username}
+         password ${password}
+        
+        `)
 
-    // 1) check if username and password are not empty
-    if (!username || !password) {
-        res.status(401).send('Please provide username and password')
+        // 1) check if username and password are not empty
+        if (!username || !password) {
+            res.status(401).send('Please provide username and password')
+        }
+
+        // 2) Check if the username exists
+        const userNameExist = (await pool.query(`select user_id, username, password from users where username=$1`, [username]));
+
+        // 2.1) if username exits then compare the coming password to the encrypted password stored in the DB 
+        const encryptedPassword = userNameExist.rows[0].password
+        const correct = await correctPassword(password, encryptedPassword)
+
+
+        if (userNameExist.rowCount === 0 || !correct) {
+            return res.status(401).json({
+                status: 'Unauthorized',
+                message: 'Incorrect username or password'
+            });
+        }
+
+        // 3 if request body not empty, and username exists and password is correct
+        const token = createJwtToken(userNameExist.rows[0].user_id)
+        const cookiesOption = {
+            expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+            secure: true,
+            httpOnly: true
+        }
+
+        if (process.env.NODE_ENV !== 'production') {
+            cookiesOption.secure = false
+        }
+
+        res.cookie('jwt', token, cookiesOption)
+
+        res.status(200).json({
+            status: 'success',
+            token
+        })
+
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+        next(err)
     }
 
-    // 2) Check if the username exists
-    const userNameExist = (await pool.query(`select user_id, username, password from users where username=$1`, [username]));
-
-    // 2.1) if username exits then compare the coming password to the encrypted password stored in the DB 
-    const encryptedPassword = userNameExist.rows[0].password
-    const correct = await correctPassword(password, encryptedPassword)
-
-
-    if (userNameExist.rowCount === 0 || !correct) {
-        return res.status(401).json({
-            status: 'Unauthorized',
-            message: 'Incorrect username or password'
-        });
-    }
-
-    // 3 if request body not empty, and username exists and password is correct
-    const token = createJwtToken(userNameExist.rows[0].user_id)
-    const cookiesOption = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        secure: true,
-        httpOnly: true
-    }
-
-    if (process.env.NODE_ENV !== 'production') {
-        cookiesOption.secure = false
-    }
-
-    res.cookie('jwt', token, cookiesOption)
-
-    res.status(200).json({
-        status: 'success',
-        token
-    })
-
-    next()
 };
 
 exports.protect = async (req, res, next) => {
