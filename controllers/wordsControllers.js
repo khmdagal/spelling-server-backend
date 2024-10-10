b5c5804403e982e131446300a44fbcdb7191b1bf
khmdagal <khmdagal@gmail.com>
2024-10-10 11:53:59 +0100

    Modify creating weekly practice

--- controllers/wordsControllers.js
+++ controllers/wordsControllers.js
@@ -16,13 +16,15 @@ exports.getWords = async (req, res, next) => {
 
 }
 
-exports.getPracticeSessions = async (req, res, next) => {
+exports.getMyWeeklyPractice = async (req, res, next) => {
+    // add here parameter of the which class
+    const { class_id } = req.params.class_id
 
     try {
-        const practiceSessions = await pool.query(`select *  from weeklypractice`)
+        const practice = await pool.query(`select *  from weeklypractice where class_id=$1`, [class_id])
         res.status(200).json({
             status: 'success',
-            data: practiceSessions.rows
+            data: practice.rows
         })
     } catch (error) {
         console.log(error)
@@ -31,27 +33,48 @@ exports.getPracticeSessions = async (req, res, next) => {
 
 }
 
-exports.createPracticeSession = async (req, res, next) => {
-    const todayDate = new Date()
-
-    const session = {
-        practice_id: String(Date.now()),
-        words: req.body.words,
-        created_at: todayDate,
-        expires_in: todayDate // fix here
+exports.getAllWeeklyPractice = async (req, res, next) => {
+    try {
+        const response = await pool.query(`select practice_id,name,description,words,school_id,class_id,created_at,expires_in from weeklypractice`)
+        res.status(200).json({
+            status: 'success',
+            data: response.rows
+        })
+    } catch (error) {
+        console.log(error)
 
     }
-    console.log(session)
-    try {
-        const practiceSession = await pool.query(`
-            insert into weeklypractice(practice_id,words,created_at,expires_in)
-            values($1,$2,$3,$4)
-            `, [session.practice_id, session.words, session.created_at, session.expires_in])
 
-        res.status(201).json({
+}
+
+
+exports.getAllWeeklyPractice = async (req, res, next) => {
+    try {
+        const result = await (await pool.query(`SELECT * FROM weeklypractice`)).rows
+
+        console.log('===>>>', result.rows)
+
+        // Send response only once
+        res.status(200).json({
             status: 'success',
-            practiceSession
-        })
+            data: result
+        });
+
+
+    } catch (error) {
+        console.log("Error fetching weekly practice:", error);
+        // next(error); // Call next to trigger error middleware if needed
+    }
+    next()
+};
+
+exports.getWeeklyPracticeById = async (req, res, next) => {
+    // this was test improve it very well
+    try {
+        const { practice_id } = req.params
+        const practiceSession = await pool.query(`select words from weeklypractice where practice_id=$1`, [practice_id]);
+        practiceSession.rows[0].map(el => res.send(el))
+
 
     } catch (error) {
         res.send(error)
@@ -59,15 +82,50 @@ exports.createPracticeSession = async (req, res, next) => {
     next()
 }
 
-exports.getPracticeSessionById = async (req, res, next) => {
-    // this was test improve it very well
-    try {
-        const { practice_id } = req.params
-        console.log(practice_id)
-        const practiceSession = await pool.query(`select words from weeklyspellingpractice where practice_id=$1`, [practice_id]);
-        console.log(practiceSession.rows[0])
-        practiceSession.rows[0].map(el => res.send(el))
 
+exports.getAllWeeklyPractice = async (req, res, next) => {
+    try {
+        const result = (await pool.query(`SELECT practice_id,name,description,words,school_id,class_id,created_at,expires_in FROM weeklypractice`)).rows
+        return res.status(200).json({
+            status: 'success',
+            result
+        })
+    } catch (error) {
+        res.send(error)
+    }
+    next()
+}
+
+exports.createWeeklyPractice = async (req, res, next) => {
+
+    const practice = {
+        name: req.body.name,
+        description: req.body.description,
+        practice_id: req.body.practice_id,
+        words: req.body.words,
+        school_id: req.body.school_id,
+        class_id: req.body.class_id,
+        created_at: req.body.created_at,
+        expires_in: req.body.expires_in
+    }
+
+    try {
+        const createdPractice = await pool.query(`
+            insert into weeklypractice(practice_id,name,description,words,school_id,class_id,created_at,expires_in)
+            values($1,$2,$3,$4,$5,$6,$7,$8)
+            `, [practice.practice_id,
+        practice.name,
+        practice.description,
+        practice.words,
+        practice.school_id,
+        practice.class_id,
+        practice.created_at,
+        practice.expires_in])
+
+        return res.status(201).json({
+            status: 'success',
+            createdPractice
+        })
 
     } catch (error) {
         res.send(error)
