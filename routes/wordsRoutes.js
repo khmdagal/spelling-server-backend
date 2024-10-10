144787f80ccd9e4902ac77395ae030a82d9b2b41
khmdagal <khmdagal@gmail.com>
2024-10-10 11:50:49 +0100

    Modified weeklypractice route

--- routes/wordsRoutes.js
+++ routes/wordsRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const { getWords, createPracticeSession, getPracticeSessions, getPracticeSessionById } = require('../controllers/wordsControllers');
+const { createWeeklyPractice, getWords, getAllWeeklyPractice } = require('../controllers/wordsControllers');
 const { protect, adminOnly } = require('../controllers/authControllers')
 const router = express.Router();
 
 
 router.route('/:yearsSelected').get(protect, getWords)
-router.route('/practice-sessions').post(adminOnly, createPracticeSession)
-router.route('/practice-sessions/:practice_id').get(protect,getPracticeSessionById)
-router.route('/practice-sessions').get(protect, getPracticeSessions)
+router.route('/weeklypractice').post(adminOnly, createWeeklyPractice)
+router.route('/weeklypractice/all').get(protect, adminOnly, getAllWeeklyPractice)
 module.exports = router
