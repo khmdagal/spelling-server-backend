08c21b530fbd2ff3575f34ecc6a9117a2d14ce2b
khmdagal <khmdagal@gmail.com>
2024-10-25 17:36:57 +0100

    Add weeklyPracticeById functionality

--- controllers/wordsControllers.js
+++ controllers/wordsControllers.js
@@ -16,85 +16,21 @@ exports.getWords = async (req, res, next) => {
 
 }
 
-exports.getMyWeeklyPractice = async (req, res, next) => {
-    // add here parameter of the which class
-    const { class_id } = req.params.class_id
-
-    try {
-        const practice = await pool.query(`select *  from weeklypractice where class_id=$1`, [class_id])
-        res.status(200).json({
-            status: 'success',
-            data: practice.rows
-        })
-    } catch (error) {
-        console.log(error)
-        next(error)
-    }
-
-}
-
 exports.getAllWeeklyPractice = async (req, res, next) => {
     try {
-        const response = await pool.query(`select practice_id,name,description,words,school_id,class_id,created_at,expires_in from weeklypractice`)
+        const result = (await pool.query(`SELECT * FROM weeklypractice`)).rows
+
         res.status(200).json({
             status: 'success',
-            data: response.rows
-        })
-    } catch (error) {
-        console.log(error)
-
-    }
-
-}
-
-
-exports.getAllWeeklyPractice = async (req, res, next) => {
-    try {
-        const result = await (await pool.query(`SELECT * FROM weeklypractice`)).rows
-
-        console.log('===>>>', result.rows)
-
-        // Send response only once
-        res.status(200).json({
-            status: 'success',
-            data: result
+            assignments: result
         });
 
 
     } catch (error) {
         console.log("Error fetching weekly practice:", error);
-        // next(error); // Call next to trigger error middleware if needed
     }
     next()
 };
-
-exports.getWeeklyPracticeById = async (req, res, next) => {
-    // this was test improve it very well
-    try {
-        const { practice_id } = req.params
-        const practiceSession = await pool.query(`select words from weeklypractice where practice_id=$1`, [practice_id]);
-        practiceSession.rows[0].map(el => res.send(el))
-
-
-    } catch (error) {
-        res.send(error)
-    }
-    next()
-}
-
-
-exports.getAllWeeklyPractice = async (req, res, next) => {
-    try {
-        const result = (await pool.query(`SELECT practice_id,name,description,words,school_id,class_id,created_at,expires_in FROM weeklypractice`)).rows
-        return res.status(200).json({
-            status: 'success',
-            result
-        })
-    } catch (error) {
-        res.send(error)
-    }
-    next()
-}
 
 exports.createWeeklyPractice = async (req, res, next) => {
 
@@ -125,6 +61,22 @@ exports.createWeeklyPractice = async (req, res, next) => {
         return res.status(201).json({
             status: 'success',
             createdPractice
+        })
+
+    } catch (error) {
+        res.send(error)
+    }
+    next()
+}
+
+exports.getWeeklyPracticeById = async (req, res, next) => {
+    try {
+        const { practice_id, school_id } = req.params
+        console.log({practice_id},{school_id})
+        const response = await pool.query(`select name,description,words from weeklypractice where practice_id=$1 and school_id=$2`, [practice_id,school_id]);
+        res.status(200).json({
+            status: 'success',
+            myAssignment: response.rows[0]
         })
 
     } catch (error) {
--- routes/wordsRoutes.js
+++ routes/wordsRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createWeeklyPractice, getWords, getAllWeeklyPractice } = require('../controllers/wordsControllers');
+const { createWeeklyPractice, getWords, getAllWeeklyPractice, getWeeklyPracticeById } = require('../controllers/wordsControllers');
 const { protect, adminOnly } = require('../controllers/authControllers')
 const router = express.Router();
 
 
-router.route('/:yearsSelected').get(protect, getWords)
+router.route('/:yearsSelected').get(adminOnly, getWords)
 router.route('/weeklypractice').post(adminOnly, createWeeklyPractice)
-router.route('/weeklypractice/all').get(protect, adminOnly, getAllWeeklyPractice)
+router.route('/weeklypractice/all').get(adminOnly, getAllWeeklyPractice)
+router.route('/myweeklypractice/:practice_id/:school_id').get(protect, getWeeklyPracticeById)
 module.exports = router
