cce4976787137c13ae73613b3d08e79d355a6999
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2025-10-27 11:54:10 +0000

    Add example route

--- routes/wordsRoutes.js
+++ routes/wordsRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createWeeklyPractice, getWords, getAllWeeklyPractice, getWeeklyPracticeById } = require('../controllers/wordsControllers');
+const { createWeeklyPractice, getWords, getAllWeeklyPractice, getWeeklyPracticeById, updateExampleSentence } = require('../controllers/wordsControllers');
 const { protect, adminOnly } = require('../controllers/authControllers')
 const router = express.Router();
 
@@ -8,4 +8,5 @@ router.route('/:yearsSelected').get(adminOnly, getWords)
 router.route('/weeklypractice').post(adminOnly, createWeeklyPractice)
 router.route('/weeklypractice/all').get(adminOnly, getAllWeeklyPractice)
 router.route('/myweeklypractice/:practice_id/:school_id').get(protect, getWeeklyPracticeById)
+router.route('/:word_id/example').put(adminOnly, updateExampleSentence)
 module.exports = router
