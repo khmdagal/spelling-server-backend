aedce57cec5b0dd35d2d6dc259c838c927f51c71
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2025-11-09 19:50:46 +0000

    Add islogin and logout routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,10 +4,13 @@ const { body } = require('express-validator');
 const { getUsers } = require('../controllers/usersControllers')
 const { signUp, logIn } = require('../controllers/authControllers')
 const { validateSignUpInput } = require('../middlewares/inputSanitazation')
-const { adminOnly } = require('../controllers/authControllers')
+const { adminOnly, isLoggedIn, logout } = require('../controllers/authControllers')
+
 
 router.route('/').get(adminOnly, getUsers)
 router.route('/signup').post(validateSignUpInput, signUp)
 router.route('/login').post(validateSignUpInput, logIn)
+router.route('/isLoggedIn').get(isLoggedIn)
+router.route('/logout').get(logout);
 
 module.exports = router
