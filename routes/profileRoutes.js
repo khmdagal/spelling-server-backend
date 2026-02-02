daad4d49e0b66c977ec5da45fe16864f09e2b0c2
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2026-02-02 21:14:27 +0000

    Update profile routes

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const {isLoggedIn, protect, adminOnly} = require ('../controllers/authControllers');
-const {createProfile,getProfile,updateProfile,deleteProfile} = require ('../controllers/profileControllers')
+const {createProfile,getProfile,updateProfile,deleteProfile , getAvatars} = require ('../controllers/profileControllers')
 
 
-router.route('/createProfile').post(isLoggedIn,protect, createProfile)
+router.route('/createProfile').post(protect, createProfile)
 router.route('/getProfile').get(protect, getProfile)
 router.route('/updateProfile/:profile_id').put(protect,updateProfile);
-router.route('deleteProfile/:profile_id').delete(adminOnly,deleteProfile);
+router.route('/getAvatars').get(protect, getAvatars)
 
 module.exports = router;
